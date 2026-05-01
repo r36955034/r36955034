3495845a81702ff042ea42eb24dcbc4537754e7b
r36955034 <r36955034@gmail.com>
2026-05-01 16:00:56 +0530

    Add files via upload

---
+++ server/assets/_tanstack-start-manifest_v-uCh1xsH2.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/ASUS/Desktop/RAJAT SURANA IMP/astral-artisan-weave-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-Dr0yryHP.js"] }, "/": { filePath: "C:/Users/ASUS/Desktop/RAJAT SURANA IMP/astral-artisan-weave-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-BLP4Re_T.js"] } }, clientEntry: "/assets/index-Dr0yryHP.js" });
+export {
+  tsrStartManifest
+};
